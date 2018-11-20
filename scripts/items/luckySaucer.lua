93ce90bc05cf7089415c3504e67e6dd020c53b50
DoctorHummer <DoctorHummer@users.noreply.github.com>
2018-11-20 15:01:16 +0400

    .

--- scripts/items/luckySaucer.lua
+++ scripts/items/luckySaucer.lua
@@ -5,6 +5,9 @@ function this:cache(player, flag)
   local player = Isaac.GetPlayer(0)
   if player:HasCollectible(this.id) then
     player:AddNullCostume(content.costumes.luckySaucer)
+    if player:HasCollectible(202) then  
+      player:ReplaceCostumeSprite(Isaac.GetItemConfig():GetNullItem(content.costumes.luckySaucer), "gfx/costumes/sheet_costume_luckySaucerG.png", 0)
+    end
     if flag == CacheFlag.CACHE_LUCK then
      player.Luck = player.Luck + 3; 
     end
