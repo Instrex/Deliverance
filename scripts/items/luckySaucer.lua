103a3a0733ef72c6a281f28608d9a47885345180
DoctorHummer <DoctorHummer@users.noreply.github.com>
2018-11-20 23:12:21 +0400

    дата хендлир взорвался

--- scripts/items/luckySaucer.lua
+++ scripts/items/luckySaucer.lua
@@ -5,17 +5,24 @@ function this:cache(player, flag)
   local player = Isaac.GetPlayer(0)
   if player:HasCollectible(this.id) then
     player:AddNullCostume(content.costumes.luckySaucer)
-    if player:HasCollectible(202) then  
-      player:ReplaceCostumeSprite(Isaac.GetItemConfig():GetNullItem(content.costumes.luckySaucer), "gfx/costumes/sheet_costume_luckySaucerG.png", 0)
-    end
     if flag == CacheFlag.CACHE_LUCK then
      player.Luck = player.Luck + 3; 
     end
   end
 end
 
+function this:update(player)
+  local player = Isaac.GetPlayer(0)
+  if player:HasCollectible(this.id) then
+    if player:HasCollectible(202) then  
+      player:ReplaceCostumeSprite(Isaac.GetItemConfig():GetNullItem(content.costumes.luckySaucer), "gfx/costumes/sheet_costume_luckySaucerG.png", 0)
+    end
+  end
+end
+
 function this.Init()
   mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, this.cache)
+  mod:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, this.update)
 end
 
 return this
