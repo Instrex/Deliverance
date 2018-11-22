1a1fe02452fe64730a2a3fec613395b0969d6626
DoctorHummer <DoctorHummer@users.noreply.github.com>
2018-11-22 10:11:00 +0400

    ouhh shet

--- scripts/entities/monsters/variants/variants.lua
+++ scripts/entities/monsters/variants/variants.lua
@@ -9,16 +9,26 @@ function this:dukieform(npc)
 end
 
 function this:lilhauntform(npc)
-  local sprite = npc:GetSprite()
-  if utils.chancep(30) and npc.Variant == 10 then
-    sprite:ReplaceSpritesheet(0,"gfx/monsters/variants/lilhaunt.png")
-    sprite:LoadGraphics()
+  local data = npc:GetData() local sprite = npc:GetSprite()
+  if utils.chancep(20) and npc.Variant == 10 then
+    data.dorky = 1
+    sprite:ReplaceSpritesheet(0,"gfx/monsters/variants/lilhaunt.png") sprite:LoadGraphics()
   end
+end
+
+function this:dorkyhauntdie(npc)
+   local data = npc:GetData()
+   if data.dorky ~= nil and npc.Variant == 10 then 
+     for i=1, 4 do
+       Isaac.Spawn(9, 0, 0, npc.Position, Vector.FromAngle(i*90):Resized(8), npc) 
+     end
+   end
 end
 
 function this.Init()
     mod:AddCallback(ModCallbacks.MC_POST_NPC_INIT, this.dukieform, 288) 
     mod:AddCallback(ModCallbacks.MC_POST_NPC_INIT, this.lilhauntform, 260) 
+    mod:AddCallback(ModCallbacks.MC_POST_NPC_DEATH, this.dorkyhauntdie, 260)
 end
 
 return this
