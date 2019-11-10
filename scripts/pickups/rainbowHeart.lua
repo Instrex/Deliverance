96757934ac46e10b79c2d5f6bac90f39fafd748c
Instrex <instrex2@gmail.com>
2019-11-10 23:21:59 +0300

    rainbow heart now spawns properly

--- scripts/pickups/rainbowHeart.lua
+++ scripts/pickups/rainbowHeart.lua
@@ -30,7 +30,7 @@ function this:updateHeart(pickup)
       local data = pickup:GetData()
       if data.change == nil then
        if pickup.SubType == HeartSubType.HEART_FULL or pickup.SubType == HeartSubType.HEART_SCARED then
-         if utils.chancep(1) and deliveranceData.persistent.unlockedRainbowHearts then
+         if utils.chancep(1) then
             
              if room:GetType() == RoomType.ROOM_SHOP then
                 local pick = Isaac.Spawn(5, 10, 4000, pickup.Position, vectorZero, nil)
