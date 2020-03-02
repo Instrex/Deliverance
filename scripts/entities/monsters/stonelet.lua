9ea08387c40526648c83f727cb7664d79be53ad9
epfly6 <36567300+epfly6@users.noreply.github.com>
2020-03-02 22:34:39 +0300

    Камень прокаченный
    🗿
    
    Co-Authored-By: baicherra <baicherra@users.noreply.github.com>

--- scripts/entities/monsters/stonelet.lua
+++ scripts/entities/monsters/stonelet.lua
@@ -3,53 +3,64 @@ this.id = Isaac.GetEntityTypeByName("Stonelet")
 this.variant = Isaac.GetEntityVariantByName("Stonelet")
 
 function this:behaviour(npc)
-	if npc.Variant == this.variant  then
-		local playertarget = npc:GetPlayerTarget()
+	if npc.Variant == this.variant then
+		local target = npc:GetPlayerTarget()
+		local data = npc:GetData()
 		local sprite = npc:GetSprite()
 		local room = game:GetRoom()
 
 		--Start--
+		if room:IsClear() then
+				npc.State = NpcState.STATE_UNIQUE_DEATH
+			end
 		if npc.State == NpcState.STATE_INIT then
 			npc.State = NpcState.STATE_MOVE
-			npc.StateFrame = 0
+			npc.StateFrame = Utils.choose(0, 10, 20)
 		--Move and Attack--	
 		elseif npc.State == NpcState.STATE_MOVE then
-			npc.Velocity = (playertarget.Position - npc.Position):Normalized() + npc.Velocity * 0.8
-			--npc.Pathfinder:FindGridPath(playertarget.Position, 0.7, 2, false)
+			if data.GridCountdown == nil then data.GridCountdown = 0 end
+			if npc:CollidesWithGrid() or data.GridCountdown > 0 then
+          		npc.Pathfinder:FindGridPath(target.Position, 0.8, 1, false)
+          		if data.GridCountdown <= 0 then
+              		data.GridCountdown = 30
+          		else
+              		data.GridCountdown = data.GridCountdown - 1
+          		end
+       		else
+			npc.Velocity = utils.vecToPos(target.Position, npc.Position) * 1 + npc.Velocity * 0.8
+		end
 			npc:AnimWalkFrame("WalkHori", "WalkVert", 0,1)
 			npc.StateFrame = npc.StateFrame + 1
-			if npc.Position:Distance (playertarget.Position) <=205 then
-				if npc.StateFrame >= 30 then
-					Isaac.Spawn(9, 0, 0, Vector(npc.Position.X, npc.Position.Y), (playertarget.Position - npc.Position):Normalized() + npc.Velocity *2, npc):ToProjectile()
-					npc.StateFrame = 0
-				end
+			if npc.Position:Distance (target.Position) <=205 and npc.StateFrame >= 45 then
+				npc.State = NpcState.STATE_ATTACK
 			end
-			if enemystatus == false then
-				npc.State = STATE_UNIQUE_DEATH 
+		elseif npc.State == NpcState.STATE_ATTACK then
+			npc.Velocity = vectorZero
+			sprite:Play("Shoot")
+			if sprite:IsEventTriggered("Roar") then
+				sfx:Play(310, 1.2, 0, false, 1)
+			end
+			if sprite:IsEventTriggered("Shoot") then
+				sfx:Play(262, 1.2, 0, false, 1)
+				Isaac.Spawn(9, 0, 0, Vector(npc.Position.X, npc.Position.Y), (utils.vecToPos(target.Position, npc.Position)*10), npc):ToProjectile()
+			end
+			if sprite:IsFinished("Shoot") then
+				npc.State = NpcState.STATE_MOVE
+				npc.StateFrame = 0
+			end
+		elseif npc.State == NpcState.STATE_UNIQUE_DEATH then
+			npc.Velocity = vectorZero
+			sprite:Play("Death")
+			if sprite:IsFinished("Death") then
+				npc:Kill()
 			end
 		end
-	end
-end
-
-
-function this:onstoneletdamage() --thx Tem ;*
-    return false
-end
-
-function this:autodead(_, npc) --thx tem (again)
-	local enemystatus=false
-	for i,entity in ipairs(Isaac.GetRoomEntities()) do
-    	if entity.Type~=600 and entity:IsEnemy() and entity:HasEntityFlags(EntityFlag.FLAG_FRIENDLY)==false then
-        	enemystatus=true
-    	end
 	end
 end
 	
 
 function this.Init()
 	mod:AddCallback(ModCallbacks.MC_NPC_UPDATE, this.behaviour, this.id)
-	mod:AddCallback(ModCallbacks.MC_PRE_NPC_UPDATE, this.autodead,this.id)
-	mod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG,this.onstoneletdamage,this.id)
 end
 
 return this
