7e7863df85a37a448d19030660af73c204ef4f92
epfly6 <36567300+epfly6@users.noreply.github.com>
2020-04-30 13:45:31 +0300

    Наработки наработочки

--- scripts/entities/monsters/explosimaw.lua
+++ scripts/entities/monsters/explosimaw.lua
@@ -49,6 +49,13 @@ function this:behaviour(npc)
     if sprite:IsEventTriggered("Explode") then
        npc.Velocity = npc.Velocity * (5+math.random(1, 3)) + Vector.FromAngle(math.random(0, 360))
        Isaac.Explode(npc.Position, npc, 1.0)
+	     for i=1, math.random(2,8) do
+		    local fire = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.RED_CANDLE_FLAME, 0, npc.Position, Vector.FromAngle(i*math.random(0,360)):Resized(9), npc)
+        if(fire.Position - target.Position):Length()<25 then
+          target:TakeDamage(1,0,EntityRef(fire),0)
+        end
+		    --prj:AddProjectileFlags(ProjectileFlags.HIT_ENEMIES)
+	     end
        sfx:Play(SoundEffect.SOUND_HELLBOSS_GROUNDPOUND , 1, 0, false, 1) 
        Game():ShakeScreen(16)
        data.Smol = true
--- scripts/items/glassCrown.lua
+++ scripts/items/glassCrown.lua
@@ -2,28 +2,82 @@ local this = {}
 this.id = Isaac.GetItemIdByName("Glass Crown")
 this.description = "Don't drop it"
 
+ local bonus = {
+  speed = 0.5,
+  damage = 5,
+  luck = 3,
+  range = 10,
+  shotspeed = 2,
+  tears = 5
+}
+
 function this:cache(player, flag)
   local player = Isaac.GetPlayer(0)
   if player:HasCollectible(this.id) then
-      if flag == CacheFlag.CACHE_SPEED then player.MoveSpeed = player.MoveSpeed + 0.5
-      elseif flag == CacheFlag.CACHE_DAMAGE then player.Damage = player.Damage + 5
-	  elseif flag == CacheFlag.CACHE_LUCK then player.Luck = player.Luck + 3
-	  elseif flag == CacheFlag.CACHE_RANGE then player.TearHeight = player.TearHeight - 10.00
-	  elseif flag == CacheFlag.CACHE_SHOTSPEED then player.ShotSpeed = player.ShotSpeed + 2
-	  elseif flag == CacheFlag.CACHE_FIREDELAY then player.MaxFireDelay = player.MaxFireDelay -5
+    if deliveranceData.temporary.glasscrowngood==nil then
+       deliveranceData.temporary.glasscrowngood=true
+       deliveranceDataHandler.directSave()
+    end
+    if flag == CacheFlag.CACHE_SPEED then player.MoveSpeed = player.MoveSpeed + bonus.speed
+    elseif flag == CacheFlag.CACHE_DAMAGE then player.Damage = player.Damage + bonus.damage
+	  elseif flag == CacheFlag.CACHE_LUCK then player.Luck = player.Luck + bonus.luck
+	  elseif flag == CacheFlag.CACHE_RANGE then player.TearHeight = player.TearHeight - bonus.range
+	  elseif flag == CacheFlag.CACHE_SHOTSPEED then player.ShotSpeed = player.ShotSpeed + bonus.shotspeed
+	  elseif flag == CacheFlag.CACHE_FIREDELAY then player.MaxFireDelay = player.MaxFireDelay - bonus.tears
 	  end
   end
 end
 
-function this.trigger(id)
+function this:update(player)
   local player = Isaac.GetPlayer(0)
   if player:HasCollectible(this.id) then
-      player:RemoveCollectible(this.id)
-      end
+    if deliveranceData.temporary.glasscrowngood==nil then
+      deliveranceData.temporary.glasscrowngood=true
+      deliveranceDataHandler.directSave()
+    end
+    if deliveranceData.temporary.glasscrowngood then
+      bonus.speed = 0.5
+      bonus.damage = 5
+      bonus.luck = 3
+      bonus.range = 10
+      bonus.shotspeed = 2
+      bonus.tears = 5
+    else
+      bonus.speed = 1
+      bonus.damage = 1
+      bonus.luck = 1
+      bonus.range = 1
+      bonus.shotspeed = 1
+      bonus.tears = 1
+    end
+    player:AddCacheFlags(CacheFlag.CACHE_ALL)
+    player:EvaluateItems()
+  end
+end
+
+
+function this:trigger(player)
+  local player = Isaac.GetPlayer(0)
+  if player:HasCollectible(this.id) then
+    if deliveranceData.temporary.glasscrowngood then
+      deliveranceData.temporary.glasscrowngood=false
+      deliveranceDataHandler.directSave()
+    end
+  end
+end
+
+function this:updateFloor()
+  local player = Isaac.GetPlayer(0)
+  local room = game:GetRoom()
+  if player:HasCollectible(this.id) then
+    deliveranceData.temporary.glasscrowngood=true
+  end
 end
 
 function this.Init()
   mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, this.cache)
+  mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, this.update)
+  mod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, this.updateFloor)
   mod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, this.trigger, EntityType.ENTITY_PLAYER)
   end
   
