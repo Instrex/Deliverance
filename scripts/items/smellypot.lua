8eb182f71523d01eea4aa451087499d9455a140d
Yaroslav <36567300+epfly6@users.noreply.github.com>
2021-06-29 14:50:09 +0300

    Smelly pot

--- scripts/items/smellypot.lua
+++ scripts/items/smellypot.lua
@@ -1,17 +1,17 @@
-local this = {}
-this.id = Isaac.GetItemIdByName("Smelly Pot")
-this.description = ""
-this.rusdescription ={""}
-this.isActive = true
+local this = {
+  id = Isaac.GetItemIdByName("Smelly Pot"),
+  description = "Spawns friendly dips",
+  rusdescription ={""},
+  isActive = true
+}
 
 function this.use()
-  local player = Isaac.GetPlayer(0)
-  local room = game:GetRoom()
-   Isaac.Spawn(1000, 34, 0, player.Position, vectorZero, player)
-    Isaac.Spawn(1000, 43, 0, player.Position, vectorZero, player)
+  local player = Isaac.GetPlayer(0) --change player get method later for proper coop support
   for i=1, math.random(1, 3) do
-   Isaac.Spawn(3, 201, math.random (0,13), Isaac.GetFreeNearPosition(player.Position, 1), Vector.FromAngle(math.random(360)):Resized(5.10), nil)
+    player:ThrowFriendlyDip(math.random(0,6), player.Position, Vector.Zero)
   end
+  Isaac.Spawn(1000, 34, 0, player.Position, vectorZero, player)
+  Isaac.Spawn(1000, 43, 0, player.Position, vectorZero, player)
 end
 
 function this.Init()
