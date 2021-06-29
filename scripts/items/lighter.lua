0507fa21a969d2df29c82b46caec524389e52ae9
Yaroslav <36567300+epfly6@users.noreply.github.com>
2021-06-29 14:29:54 +0300

    Спасаю

--- scripts/items/lighter.lua
+++ scripts/items/lighter.lua
@@ -6,11 +6,11 @@ this.isActive = true
 
 function this.use()
   sfx:Play(Isaac.GetSoundIdByName("Firestarter") , 0.75, 0, false, 1)
-  for e, entity in pairs(Isaac.GetRoomEntities()) do 
+  for e, entity in pairs(Isaac.GetRoomEntities()) do
      if entity:IsActiveEnemy() and not entity:HasEntityFlags(EntityFlag.FLAG_FRIENDLY) and not entity:HasEntityFlags(EntityFlag.FLAG_NO_TARGET) and entity:IsVulnerableEnemy() then 
-        entity:AddBurn(EntityRef(nil), 1800, 250) 
+        entity:AddBurn(EntityRef(nil), 1800,2.15 + 0.20 * (game:GetLevel():GetStage() - 1))
      end 
-     if entity.Type == 1000 and (entity.Variant == 26 or entity.Variant == 45) then 
+     if entity.Type == 1000 and (entity.Variant == 26 or entity.Variant == 45) then
         local fsize = 0.5+math.random()*0.5
         local fire = Isaac.Spawn(1000, Isaac.GetEntityVariantByName("Gasoline Fire"), 0, entity.Position, vectorZero, player)
         local data = fire:GetData()
